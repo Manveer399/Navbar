36f98d51f3309d9e315ae19920df96b874f47e9b
Manveer Kasseran <Manveer399@users.noreply.github.com>
2022-01-06 21:50:38 +0800

    added styling and hamburger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+/*if the  active class does not exists it will add it, and if it exists it will remove*/
